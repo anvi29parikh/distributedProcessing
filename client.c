c62a89ebaf8696d93ddac93b8cd43520bbee7f79
Nischay Dhiman <nischay.dhiman@gmail.com>
2022-04-25 21:06:19 -0400

    adding client & server code changes for grep

--- client.c
+++ client.c
@@ -9,20 +9,22 @@
 #include <sys/types.h>
 #include <sys/signal.h>
 
+
+
 int main(int argc, char *argv[]){
 	char message[255];
 	int ssd, portNumber;
 	socklen_t len;
 	struct sockaddr_in servAdd;
 
-	if(argc != 5){
-	printf("Call model:%s <IP> <Port#> <File1> <File2>\n",argv[0]);
-	exit(0);
+	if(argc != 6){
+		printf("Call model:%s <IP> <Port#> <Pattern> <File1> <File2>\n",argv[0]);
+		exit(0);
 	}
 	
 	if((ssd = socket(AF_INET, SOCK_STREAM, 0))<0){
-	fprintf(stderr, "Cannot create socket\n");
-	exit(1);
+		fprintf(stderr, "Cannot create socket\n");
+		exit(1);
 	}
 
 	servAdd.sin_family = AF_INET;
@@ -30,38 +32,57 @@ int main(int argc, char *argv[]){
 	servAdd.sin_port = htons((uint16_t)portNumber);//convert host bytestream into network bytestream
 
 	if(inet_pton(AF_INET,argv[1],&servAdd.sin_addr)<0){
-	fprintf(stderr, " inet_pton() has failed\n");
-	exit(2);
+		fprintf(stderr, " inet_pton() has failed\n");
+		exit(2);
 	}
-
+	
 
 	if(connect(ssd,(struct sockaddr *)&servAdd,sizeof(servAdd))<0){
-	fprintf(stderr, "connect() has failed, exiting\n");
-	exit(3);
+		fprintf(stderr, "connect() has failed, exiting\n");
+		exit(3);
 	}
-
-
-
-	//while(1){
-		int fd = open(argv[4], O_RDONLY);
-		fprintf(stderr, "Enter the $ sign to quit or a message for the server\n");
+	
+	
+	int pid=fork();
+    
+	if(pid==0)
+	{
+	    printf("Local client output: \n");
+	    char *command_args[] = { "/usr/bin/grep", "-w", argv[3], "--color=always", argv[4], NULL };
+	    execv(command_args[0], command_args);
+	    exit(0);
+	}
+	else if(pid>0)
+	{
+		waitpid(pid, NULL, 0);
+	  int fd = open(argv[5], O_RDONLY);
 		char fileContent[255];
 		int n = 255;
 		int len_f = lseek(fd,0,SEEK_END);
 		lseek(fd,0,SEEK_SET);
+		write(ssd, argv[3], strlen(argv[3])+1);
 		while(n!=0){
-			
-			printf("len:%d\n",len_f);
+			// printf("len:%d\n",len_f);
 			n=read(fd, fileContent,255);
 			if((len_f+1)<255){
 				n = 0;
 			}
-			fprintf(stderr, "file's message: %s:::%d\n",fileContent,n);
+			// fprintf(stderr, "file's message: %s:::%d\n",fileContent,n);
 			write(ssd, fileContent, strlen(fileContent)+1);
 		}
 		close(fd);
-		fprintf(stderr, "written");
-	//}
+
+		fprintf(stderr, "written to server, waiting for results : \n");
+
+		if(read(ssd, message, 255)<0){
+			fprintf(stderr, "read() error\n");
+			exit(3);
+		}
+		fprintf(stderr, "server's Output: \n%s ",message);
+		
+	}
+	
+	
 }
 
 
--- server.c
+++ server.c
@@ -3,44 +3,12 @@
 #include <netinet/in.h>
 #include <stdlib.h>
 #include <string.h>
-#include<fcntl.h>
+#include <fcntl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/signal.h>
 static int clients = 0;
-int main(int argc, char *argv[]){
-	int sd, csd, portNumber;
-	socklen_t len;
-	struct sockaddr_in servAdd;
 
-	if(argc != 2){
-	printf("Call model: %s <Port #>\n", argv[0]);
-	exit(0);
-	}
-
-	if ((sd=socket(AF_INET,SOCK_STREAM,0))<0){
-	fprintf(stderr, "Cannot create socket\n");
-	exit(1);
-	}
-
-	servAdd.sin_family = AF_INET;
-	servAdd.sin_addr.s_addr = htonl(INADDR_ANY);
-	sscanf(argv[1], "%d", &portNumber);
-	servAdd.sin_port = htons((uint16_t)portNumber);
-
-	bind(sd,(struct sockaddr*)&servAdd,sizeof(servAdd));
-
-	listen(sd, 5);
-
-	while(1){
-	csd=accept(sd,(struct sockaddr*)NULL,NULL);
-	printf("New connection with client %d\n", clients);
-	if(fork()==0)
-	child(csd, clients);
-	
-	}
-	close(csd);
-}
 
 void child(int sd, int clients){
 	char fileName[32];
@@ -53,15 +21,53 @@ void child(int sd, int clients){
 		fprintf(stderr, "client's message: %s\n",fileContent);
 		int n=write(fd,fileContent,255);
 		fprintf(stderr,"Read bytes %d\n", n);
+		dup2(sd, 1);
+		char *command_args[] = { "/usr/bin/grep", "-w", "Client", "--color=always", "README.md", NULL };
+    // # how to output the result to socket
+		execv(command_args[0], command_args);
+		dup2(1, 1);
 	}else{
 		fprintf(stderr, "not able to read\n");
 	}
 	close(fd);
 	close(sd);
 	fprintf(stderr,"Client is dead, wait for a new client\n");
-		exit(0);
+	exit(0);
 }
 
+
+int main(int argc, char *argv[]){
+	int sd, csd, portNumber;
+	socklen_t len;
+	struct sockaddr_in servAdd;
+
+	if(argc != 2){
+		printf("Call model: %s <Port #>\n", argv[0]);
+		exit(0);
+	}
+
+	if ((sd=socket(AF_INET,SOCK_STREAM,0))<0){
+		fprintf(stderr, "Cannot create socket\n");
+		exit(1);
+	}
+
+	servAdd.sin_family = AF_INET;
+	servAdd.sin_addr.s_addr = htonl(INADDR_ANY);
+	sscanf(argv[1], "%d", &portNumber);
+	servAdd.sin_port = htons((uint16_t)portNumber);
+
+	bind(sd,(struct sockaddr*)&servAdd,sizeof(servAdd));
+
+	listen(sd, 5);
+
+	while(1){
+		csd=accept(sd,(struct sockaddr*)NULL,NULL);
+		printf("New connection with client %d\n", clients);
+		if(fork()==0)
+			child(csd, clients);
+	}
+	close(csd);
+}
 
 
 
