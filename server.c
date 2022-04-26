89f98110af86c2ef924944deb4758a0eb0e689ff
Nischay Dhiman <nischay.dhiman@gmail.com>
2022-04-25 21:31:36 -0400

    add server changes for dynamic files

--- server.c
+++ server.c
@@ -7,28 +7,46 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/signal.h>
+#include <time.h>
+#include <stdlib.h>
+
 static int clients = 0;
+
 
 
 void child(int sd, int clients){
 	char fileName[32];
-	snprintf(fileName, sizeof(char) * 32, "file%i.txt", clients);
+	srand(time(NULL));
+	int random_filename = rand();
+	snprintf(fileName, sizeof(char) * 32, "file_%i.txt", random_filename);
 
-	int fd = open(fileName, O_CREAT|O_RDWR, 0777);
+	int fd = open(fileName, O_CREAT|O_RDWR, 0666);
 	char fileContent[255];
 	fprintf(stderr,"Now reading content\n");
-	if(read(sd, fileContent, 255)>0){
-		fprintf(stderr, "client's message: %s\n",fileContent);
-		int n=write(fd,fileContent,255);
-		fprintf(stderr,"Read bytes %d\n", n);
-		dup2(sd, 1);
-		char *command_args[] = { "/usr/bin/grep", "-w", "Client", "--color=always", "README.md", NULL };
-    // # how to output the result to socket
-		execv(command_args[0], command_args);
-		dup2(1, 1);
+	char pattern[10];
+	if(read(sd, pattern, 10)>0){
+		fprintf(stderr, "client's pattern: %s\n",pattern);
+		if(read(sd, fileContent, 255)>0){
+			fprintf(stderr, "client's message: %s\n",fileContent);
+			int n=write(fd,fileContent,255);
+			fprintf(stderr,"Read bytes %d\n", n);
+			
+			// pipe 
+			dup2(sd, 1);
+			char *command_args[] = { "grep", "--text", "-w", pattern, "--color=always", fileName, NULL };
+	    // # how to output the result to socket
+	    
+	    		printf("- before execv");
+			execvp(command_args[0], command_args);
+			
+		}else{
+			fprintf(stderr, "not able to read\n");
+		}
 	}else{
-		fprintf(stderr, "not able to read\n");
+		fprintf(stderr, "not able to read pattern\n");
 	}
+	
+	
 	close(fd);
 	close(sd);
 	fprintf(stderr,"Client is dead, wait for a new client\n");
@@ -37,6 +55,7 @@ void child(int sd, int clients){
 
 
 int main(int argc, char *argv[]){
+	
 	int sd, csd, portNumber;
 	socklen_t len;
 	struct sockaddr_in servAdd;
@@ -68,14 +87,4 @@ int main(int argc, char *argv[]){
 	}
 	close(csd);
 }
-
-
-
-
-
-
-
-
-
-
 
